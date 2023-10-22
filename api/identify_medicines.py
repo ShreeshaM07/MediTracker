522d5ef50804d7132f8ee536ba92322a376dcc43
Shreesha M <shreesha2k22@yahoo.com>
2023-10-23 01:01:17 +0530

    made the medicine_info_screen into an user editable screen while also updating the tabList stored as editedTabList

--- api/identify_medicines.py
+++ api/identify_medicines.py
@@ -7,75 +7,6 @@ def findFollowUpDate(extracted_text):
     follow_up_date = follow_up_match.group(1) if follow_up_match else None
     return follow_up_date
 
-# Example usage
-#extracted_text = "Hospital Name\n\nAFHJVN;BFOP;BGP;NGLJDFGLS;\n\nPhn:2389u4y0\n\nPatient Namezwipgths\nAge:s8\n\nSexson\n\nInvestigations: xray,dghibif\nDiagnosixifizhul\n\nMedication:\n\nMedicine name Dosage Duration\n\nTab, ibuprofen \u20181 morning, 1 night (Before food) 3 days\nCap, omez, 1 morning , 1 night (before food) 3 days\nOintment, Volini afternoon (after food) 2 days\n\nAdvice: take medicines on time\n\nFollow up: 12/01/22\n\n\n\n\n\n"
-
-
-def extractTablets(extracted_text):
-    #extracted_text="Hospital Name |\n\nAFHJVN;BFOP;BGP;NGLIDFGLS;\n\nPhn:2389u4y0\n\nPatient Name:ruipgthg\nAge:88\nsexim\nInvestigations: xray,dghibif\n\nDiagnosis:ifjighuj\n\nMedication:\n\nMedicine name Dosage Duration\nTab, ibuprofen \u20181 morning, 1 night (Before food) 3 days\nCap, omez \u2018L morning , 1 night (before food) 3 days\nOintment, Volini afternoon (after food) 2 days\n\nAdvice: take medicines on time\n\nFollow up: 12/01/22\n\nDname\n\njfapbgliabg\n"
-    sentences=list(extracted_text.split('\n'))
-    #print(sentences)
-    line_no=None
-    for i in range(len(sentences)):
-        if 'Medicine name Dosage' in sentences[i]:
-            line_no=i
-            break
-    tablets_arr=[]
-    line_no+=1
-    while(line_no<len(sentences)):
-        
-        # if(sentences[line_no][0]!='Cap' or sentences[line_no][0]!='Capsule' or sentences[line_no][0]!='Tab' or sentences[line_no][0]!='Tablet' or sentences[line_no][0]!='ointment'):
-        #     break
-        tablets_arr.append(sentences[line_no])
-        line_no+=1
-        
-
-    #print(tablets_arr)
-    #tab_list is the final list that will be sent back to the flutter
-    tab_list=[]
-    for i in range(len(tablets_arr)):
-        tab_sentence=[]
-        try:
-            word_in_line=list(tablets_arr[i].split())
-            #print(word_in_line)
-            if(word_in_line[0]=='Tab,' or word_in_line[0]=='Cap,' or word_in_line[0]=='Ointment,' ):
-                word_in_line[0]+=word_in_line[1]
-                #print(word_in_line)
-                tab_sentence.append(word_in_line[0])
-                if 'morning' in word_in_line or 'Morning' in word_in_line or 'morning,' in word_in_line or 'Morning,' in word_in_line:
-                    tab_sentence.append('morning')
-                if 'afternoon' in word_in_line or 'Afternoon' in word_in_line or 'afternoon,' in word_in_line or 'Afternoon,' in word_in_line:
-                    tab_sentence.append('afternoon')
-                if 'evening' in word_in_line or 'Evening' in word_in_line or 'evening,' in word_in_line or 'Evening,' in word_in_line:
-                    tab_sentence.append('evening')
-                if 'night' in word_in_line or 'Night' in word_in_line or 'night,' in word_in_line or 'Night,' in word_in_line:
-                    tab_sentence.append('night')
-                if '(before' in word_in_line or '(Before' in word_in_line or 'before' in word_in_line or 'Before' in word_in_line:
-                    tab_sentence.append('before')
-                elif '(after' in word_in_line or '(After' in word_in_line or 'after' in word_in_line or 'After' in word_in_line:
-                    tab_sentence.append('after')
-                for j in range(len(word_in_line)):
-                    if(word_in_line[j]=='days' or word_in_line[j]=='day'):
-                        tab_sentence.append(word_in_line[j-1])
-                
-                
-                    
-
-            else:
-                break
-            
-        except Exception :
-            continue
-            
-        #print(tab_sentence)
-        tab_list.append(tab_sentence)
-    follow_up_date = findFollowUpDate(extracted_text)
-
-    print("Follow-up Date:", follow_up_date)
-    
-    tab_list.append(['Follow up date',follow_up_date])
-    print(tab_list)
-    return tab_list
     
 def extract_Tablets(extracted_text):
     sentences=list(extracted_text.split('\n'))
@@ -92,24 +23,118 @@ def extract_Tablets(extracted_text):
             continue
     follow_up_date = findFollowUpDate(extracted_text)
     for i in range(len(tablets_list)):
-        if 'morning' in tablets_list[i][0] or 'Morning' in tablets_list[i][0]:
-            tablets_list[i].append('morning')
-        if 'afternoon' in tablets_list[i][0] or 'Afternoon' in tablets_list[i][0]:
-            tablets_list[i].append('afternoon')
-        if 'evening' in tablets_list[i][0] or 'Evening' in tablets_list[i][0]:
-            tablets_list[i].append('evening')
-        if 'night' in tablets_list[i][0] or 'Night' in tablets_list[i][0]:
-            tablets_list[i].append('night')
+        before_after=0
         if 'before' in tablets_list[i][0] or 'Before' in tablets_list[i][0]:
-            tablets_list[i].append('before')
+            before_after=1
+            #1 indicates before food
+            #0 indicates after food
         else:
-            tablets_list[i].append('after')
+            before_after=0
+        if 'morning' in tablets_list[i][0] or 'Morning' in tablets_list[i][0]:
+            if(before_after==1):
+                tablets_list[i].append('before morning')
+            else:
+                tablets_list[i].append('after morning')
+        if 'afternoon' in tablets_list[i][0] or 'Afternoon' in tablets_list[i][0]:
+            if(before_after==1):
+                tablets_list[i].append('before afternoon')
+            else:
+                tablets_list[i].append('after afternoon')
+        if 'evening' in tablets_list[i][0] or 'Evening' in tablets_list[i][0]:
+            if(before_after==1):
+                tablets_list[i].append('before evening')
+            else:
+                tablets_list[i].append('after evening')
+        if 'night' in tablets_list[i][0] or 'Night' in tablets_list[i][0]:
+            if(before_after==1):
+                tablets_list[i].append('before night')
+            else:
+                tablets_list[i].append('after night')
+        
+        if 'days' in tablets_list[i][0] or 'day' in tablets_list[i][0]:
+            len_days_string=len(tablets_list[i][0])
+            no_days=tablets_list[i][0][len_days_string-6:len_days_string-4].strip()
+            tablets_list[i].append(no_days)
         
     tablets_list.append(['Follow up date',follow_up_date])
     print("Follow-up Date:", follow_up_date)
     print(tablets_list)
     return tablets_list
+
+extracted_text="Hospital Name |\n\nAFHJVN;BFOP;BGP;NGLIDFGLS;\n\nPhn:2389u4y0\n\nPatient Name:ruipgthg\nAge:88\nsexim\nInvestigations: xray,dghibif\n\nDiagnosis:ifjighuj\n\nMedication:\n\nMedicine name Dosage\n\nDuration\nTab, ibuprofen \u20181 morning, 1 night (Before food) 3 days\nCap, omez \u2018L morning , 1 night (before food) 3 days\nOintment, Volini afternoon (after food) 2 days\n\nAdvice: take medicines on time\n\nFollow up: 12/01/22\n\nDname\n\njfapbgliabg\n"
+extract_Tablets(extracted_text)        
+    
+
+# Example usage
+#extracted_text = "Hospital Name\n\nAFHJVN;BFOP;BGP;NGLJDFGLS;\n\nPhn:2389u4y0\n\nPatient Namezwipgths\nAge:s8\n\nSexson\n\nInvestigations: xray,dghibif\nDiagnosixifizhul\n\nMedication:\n\nMedicine name Dosage Duration\n\nTab, ibuprofen \u20181 morning, 1 night (Before food) 3 days\nCap, omez, 1 morning , 1 night (before food) 3 days\nOintment, Volini afternoon (after food) 2 days\n\nAdvice: take medicines on time\n\nFollow up: 12/01/22\n\n\n\n\n\n"
+
+
+# def extractTablets(extracted_text):
+#     #extracted_text="Hospital Name |\n\nAFHJVN;BFOP;BGP;NGLIDFGLS;\n\nPhn:2389u4y0\n\nPatient Name:ruipgthg\nAge:88\nsexim\nInvestigations: xray,dghibif\n\nDiagnosis:ifjighuj\n\nMedication:\n\nMedicine name Dosage Duration\nTab, ibuprofen \u20181 morning, 1 night (Before food) 3 days\nCap, omez \u2018L morning , 1 night (before food) 3 days\nOintment, Volini afternoon (after food) 2 days\n\nAdvice: take medicines on time\n\nFollow up: 12/01/22\n\nDname\n\njfapbgliabg\n"
+#     sentences=list(extracted_text.split('\n'))
+#     #print(sentences)
+#     line_no=None
+#     for i in range(len(sentences)):
+#         if 'Medicine name Dosage' in sentences[i]:
+#             line_no=i
+#             break
+#     tablets_arr=[]
+#     line_no+=1
+#     while(line_no<len(sentences)):
         
+#         # if(sentences[line_no][0]!='Cap' or sentences[line_no][0]!='Capsule' or sentences[line_no][0]!='Tab' or sentences[line_no][0]!='Tablet' or sentences[line_no][0]!='ointment'):
+#         #     break
+#         tablets_arr.append(sentences[line_no])
+#         line_no+=1
+        
+
+#     #print(tablets_arr)
+#     #tab_list is the final list that will be sent back to the flutter
+#     tab_list=[]
+#     for i in range(len(tablets_arr)):
+#         tab_sentence=[]
+#         try:
+#             word_in_line=list(tablets_arr[i].split())
+#             #print(word_in_line)
+#             if(word_in_line[0]=='Tab,' or word_in_line[0]=='Cap,' or word_in_line[0]=='Ointment,' ):
+#                 word_in_line[0]+=word_in_line[1]
+#                 #print(word_in_line)
+#                 tab_sentence.append(word_in_line[0])
+#                 if 'morning' in word_in_line or 'Morning' in word_in_line or 'morning,' in word_in_line or 'Morning,' in word_in_line:
+#                     tab_sentence.append('morning')
+#                 if 'afternoon' in word_in_line or 'Afternoon' in word_in_line or 'afternoon,' in word_in_line or 'Afternoon,' in word_in_line:
+#                     tab_sentence.append('afternoon')
+#                 if 'evening' in word_in_line or 'Evening' in word_in_line or 'evening,' in word_in_line or 'Evening,' in word_in_line:
+#                     tab_sentence.append('evening')
+#                 if 'night' in word_in_line or 'Night' in word_in_line or 'night,' in word_in_line or 'Night,' in word_in_line:
+#                     tab_sentence.append('night')
+#                 if '(before' in word_in_line or '(Before' in word_in_line or 'before' in word_in_line or 'Before' in word_in_line:
+#                     tab_sentence.append('before')
+#                 elif '(after' in word_in_line or '(After' in word_in_line or 'after' in word_in_line or 'After' in word_in_line:
+#                     tab_sentence.append('after')
+#                 for j in range(len(word_in_line)):
+#                     if(word_in_line[j]=='days' or word_in_line[j]=='day'):
+#                         tab_sentence.append(word_in_line[j-1])
+                
+                
+                    
+
+#             else:
+#                 break
+            
+#         except Exception :
+#             continue
+            
+#         #print(tab_sentence)
+#         tab_list.append(tab_sentence)
+#     follow_up_date = findFollowUpDate(extracted_text)
+
+#     print("Follow-up Date:", follow_up_date)
+    
+#     tab_list.append(['Follow up date',follow_up_date])
+#     print(tab_list)
+#     return tab_list
+
     
 
 
@@ -117,5 +142,3 @@ def extract_Tablets(extracted_text):
      
 # extractTablets(extracted_text)
 
-# extracted_text="Hospital Name |\n\nAFHJVN;BFOP;BGP;NGLIDFGLS;\n\nPhn:2389u4y0\n\nPatient Name:ruipgthg\nAge:88\nsexim\nInvestigations: xray,dghibif\n\nDiagnosis:ifjighuj\n\nMedication:\n\nMedicine name Dosage\n\nDuration\nTab, ibuprofen \u20181 morning, 1 night (Before food) 3 days\nCap, omez \u2018L morning , 1 night (before food) 3 days\nOintment, Volini afternoon (after food) 2 days\n\nAdvice: take medicines on time\n\nFollow up: 12/01/22\n\nDname\n\njfapbgliabg\n"
-# extract_Tablets(extracted_text)
--- lib/medicine_info_screen.dart
+++ lib/medicine_info_screen.dart
@@ -1,9 +1,130 @@
 import 'package:flutter/material.dart';
 
-class MedicineInfoScreen extends StatelessWidget {
+class MedicineInfoScreen extends StatefulWidget {
   final List<List<String>> tabList;
 
   MedicineInfoScreen({required this.tabList});
+
+  @override
+  _MedicineInfoScreenState createState() => _MedicineInfoScreenState();
+}
+
+class _MedicineInfoScreenState extends State<MedicineInfoScreen> {
+  List<List<String>> editedTabList = [];
+
+  @override
+  void initState() {
+    super.initState();
+    // Initialize editedTabList with the initial data
+    editedTabList.addAll(widget.tabList);
+  }
+
+  void _addMedicine() async {
+    TextEditingController _controller = TextEditingController();
+    String? newData = await showDialog(
+      context: context,
+      builder: (BuildContext context) {
+        return AlertDialog(
+          title: Text(
+              'Add New Medicine with timings and \nduration in days in separate lines'),
+          content: Column(
+            children: [
+              TextField(
+                controller: _controller,
+                maxLines: null,
+                keyboardType: TextInputType.multiline,
+              ),
+              SizedBox(height: 8),
+              Text(
+                'Note: While adding new elements or editing existing ones, keep the same format in mind of using "after" or "before" before the time of the day.',
+                style: TextStyle(color: Colors.grey),
+              ),
+            ],
+          ),
+          actions: <Widget>[
+            TextButton(
+              onPressed: () => Navigator.of(context).pop(),
+              child: Text('Cancel'),
+            ),
+            ElevatedButton(
+              onPressed: () {
+                // Save the new data and pop the dialog
+                Navigator.of(context).pop(_controller.text);
+              },
+              child: Text('Add'),
+            ),
+          ],
+        );
+      },
+    );
+
+    if (newData != null && newData.isNotEmpty) {
+      setState(() {
+        // Add the new medicine to the editedTabList
+        editedTabList.insert(editedTabList.length - 1, newData.split('\n'));
+
+        // Print editedTabList in the terminal
+        print('Edited List: $editedTabList');
+      });
+    }
+  }
+
+  void _editMedicine(int index) async {
+    String initialData = editedTabList[index].join('\n');
+    TextEditingController _controller =
+        TextEditingController(text: initialData);
+    String? updatedData = await showDialog(
+      context: context,
+      builder: (BuildContext context) {
+        return AlertDialog(
+          title: Text('Edit Medicine'),
+          content: Column(
+            children: [
+              TextField(
+                controller: _controller,
+                maxLines: null,
+                keyboardType: TextInputType.multiline,
+              ),
+              SizedBox(height: 8),
+              Text(
+                'Note: While adding new elements or editing existing ones, keep the same format in mind of using "after" or "before" before the time of the day.',
+                style: TextStyle(color: Colors.grey),
+              ),
+            ],
+          ),
+          actions: <Widget>[
+            TextButton(
+              onPressed: () => Navigator.of(context).pop(),
+              child: Text('Cancel'),
+            ),
+            ElevatedButton(
+              onPressed: () {
+                // Save the updated data and pop the dialog
+                Navigator.of(context).pop(_controller.text);
+              },
+              child: Text('Save'),
+            ),
+          ],
+        );
+      },
+    );
+
+    if (updatedData != null) {
+      setState(() {
+        // Update the editedTabList with the new data
+        editedTabList[index] = updatedData.split('\n');
+        print('Edited tabList: $editedTabList');
+      });
+    }
+  }
+
+  void _deleteMedicine(int index) {
+    setState(() {
+      // Remove the medicine from editedTabList at the given index
+      editedTabList.removeAt(index);
+      print('Edited tabList after deletion: $editedTabList');
+    });
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -13,10 +134,10 @@ class MedicineInfoScreen extends StatelessWidget {
         centerTitle: true,
       ),
       body: ListView.builder(
-        itemCount: tabList.length,
+        itemCount: editedTabList.length,
         itemBuilder: (context, index) {
-          String medicineName = tabList[index][0];
-          List<String> additionalInfo = tabList[index].sublist(1);
+          String medicineName = editedTabList[index][0];
+          List<String> additionalInfo = editedTabList[index].sublist(1);
 
           // Build subtitle text with all available information
           String subtitleText = additionalInfo.join('\n');
@@ -24,8 +145,31 @@ class MedicineInfoScreen extends StatelessWidget {
           return ListTile(
             title: Text(medicineName),
             subtitle: Text(subtitleText),
+            trailing: Row(
+              mainAxisSize: MainAxisSize.min,
+              children: [
+                IconButton(
+                  icon: Icon(Icons.edit),
+                  onPressed: () {
+                    // Handle edit button tap
+                    _editMedicine(index);
+                  },
+                ),
+                IconButton(
+                  icon: Icon(Icons.delete),
+                  onPressed: () {
+                    // Handle delete button tap
+                    _deleteMedicine(index);
+                  },
+                ),
+              ],
+            ),
           );
         },
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: _addMedicine,
+        child: Icon(Icons.add),
       ),
     );
   }
